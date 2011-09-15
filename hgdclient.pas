6333e8ad6d0da682edc485fc4eeeb5730c920cbe
Tristan Linnell <talinnell@gmail.com>
2011-09-15 16:33:10 +0100

    Add licence to all source files and in COPYING.

--- hgdclient.pas
+++ hgdclient.pas
@@ -1,9 +1,22 @@
-//
-// Copyright 2011 Tristan Linnell
-//    tris@canthack.org
-//
-// HGDClient.pas - HGD Client in freepascal/Lazarus
-//      Uses the synapse networking library
+/*
+ * Copyright (c) 2011, Tristan Linnell <tris@canthack.org>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+// hgdc-x Cross Platform hgd client written in Lazarus/Freepascal
+// HGDClient.pas - HGD Client class
+// Uses the synapse networking library
 
 unit HGDClient;
 
